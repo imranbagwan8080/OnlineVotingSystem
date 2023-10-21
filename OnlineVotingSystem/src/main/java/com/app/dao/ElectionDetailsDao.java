6cb786880b7623b4d91f598df337dee0ce9c949a
Pratik272000 <pkoli4470@gmail.com>
2023-10-22 01:51:07 +0530

    Todays Election list Api and Get list of Election Names

--- OnlineVotingSystem/src/main/java/com/app/dao/ElectionDetailsDao.java
+++ OnlineVotingSystem/src/main/java/com/app/dao/ElectionDetailsDao.java
@@ -1,9 +1,13 @@
 package com.app.dao;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.app.entities.ElectionDetails;
 
 public interface ElectionDetailsDao extends JpaRepository<ElectionDetails, Long> {
-
+ElectionDetails findByEndDate(Date endDate);
+List<ElectionDetails> findAll();
 }
